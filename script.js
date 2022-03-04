1d61e513fb4cf5783f2d38903d16fbebeae4cfbc
“mdreu” <mdreusi@gmail.com>
2022-03-04 09:59:18 -0500

    IfitWorksItWorks

---
+++ script.js
@@ -0,0 +1,20 @@
+let randomNum = Math.floor(1 + Math.random()*5)
+
+if ( randomNum >= 4) {
+ console.log("Greater or equal to 4");
+}
+else if (randomNum == 2 || randomNum == 3 ) {
+ console.log("Equal to 2 or 3");
+}
+else {
+ console.log("Equal to One");
+}
+if ( randomNum /= 3) {
+ console.log("Not equal to 3");
+}
+if ( randomNum !== 3 & randomNum !== 5 ) {
+    console.log("Not equal to 3 And not equal to 5");
+   }
+if (randomNum == 2 || randomNum == 4 ) {
+   console.log("Equal to 2 or 4");
+}   
